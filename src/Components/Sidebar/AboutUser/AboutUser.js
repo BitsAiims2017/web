391330b91c4d8caed9b16ab5d0a8a8c27b820337
Ayush Sharma <code.ayush@gmail.com>
2017-06-23 22:32:53 +0530

    Reduce code duplication with stateless components

--- src/Components/Sidebar/AboutUser/AboutUser.js
+++ src/Components/Sidebar/AboutUser/AboutUser.js
@@ -1,45 +1,21 @@
 import React from "react";
 import "./AboutUser.css";
 
-export default class AboutUser extends React.Component {
-    defaultProps = {
-        userInfo: {
-            token: localStorage["aiims-login-token"],
-            username: localStorage["aiims-login-username"],
-            role: localStorage["aiims-login-role"]
-        }
-    };
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            userInfo: {
-                token: "",
-                username: "",
-                role: ""
-            }
-        };
-    }
-
-    componentWillMount() {
-        this.setState({
-            userInfo: {
-                token: localStorage["aiims-login-token"],
-                username: localStorage["aiims-login-username"],
-                role: localStorage["aiims-login-role"]
-            }
-        });
-    }
-
-    render() {
-        return (
-            <div className="AboutUser">
-                <div className="Username">
-                    <h2>{this.state.userInfo.username.replace(/^"/, "").replace(/"$/, "")}</h2>
-                    <h4>{this.state.userInfo.role.replace(/^"/, "").replace(/"$/, "")}</h4>
-                </div>
+const AboutUser = props => {
+    return (
+        <div className="AboutUser">
+            <div className="Username">
+                <h2>
+                    {props.userInfo.username
+                        .replace(/^"/, "")
+                        .replace(/"$/, "")}
+                </h2>
+                <h4>
+                    {props.userInfo.role.replace(/^"/, "").replace(/"$/, "")}
+                </h4>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default AboutUser;
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -3,27 +3,13 @@ import "./Sidebar.css";
 import AboutUser from "./AboutUser/AboutUser";
 import Logout from "./Logout/Logout";
 
-export default class Sidebar extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const Sidebar = props => {
+    return (
+        <div className="Sidebar">
+            <AboutUser userInfo={props.userInfo} />
+            <Logout forgetUserInfo={props.forgetUserInfo} />
+        </div>
+    );
+};
 
-    componentWillMount() {
-        this.setState({
-            userInfo: {
-                token: localStorage["aiims-login-token"],
-                username: localStorage["aiims-login-username"],
-                role: localStorage["aiims-login-role"]
-            }
-        });
-    }
-
-    render() {
-        return (
-            <div className="Sidebar">
-                <AboutUser userInfo={this.props.userInfo} />
-                <Logout forgetUserInfo={this.props.forgetUserInfo} />
-            </div>
-        );
-    }
-}
+export default Sidebar;
